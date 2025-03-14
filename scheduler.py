46a440a0dceeaa3cff474c663783fd86cd264d67
atnatiyos <39485678+atnatiyos@users.noreply.github.com>
2025-03-13 20:32:28 -1200

    scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -6,7 +6,7 @@ def run_script():
     subprocess.run(["python", "scrap.py"])
 
 # Schedule the script to run every Monday at 10 AM
-schedule.every(1).minutes.do(run_script)
+schedule.every(1).days.do(run_script)
 
 while True:
     schedule.run_pending()
